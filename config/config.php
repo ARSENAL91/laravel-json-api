b619b255b6bd08080ac2cd07feb2778e52262e3b
jie-mini <>
2021-01-22 09:17:58 +0800

    josn-api

--- config/config.php
+++ config/config.php
@@ -2,12 +2,11 @@
 
 return [
     'method_not_allowed' => [
-        'code'   => 10000,
+        'code' => 10000,
         'detail' => '请求方法不允许',
     ],
-
     'not_found' => [
-        'code'   => 10001,
+        'code' => 10001,
         'detail' => '资源未找到',
     ],
 ];
